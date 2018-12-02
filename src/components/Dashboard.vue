abdef03ccfb0e26ef0662d83a606f7f2dcab66ec
mehdi-aouadi <mehdi.aouadi@gmail.com>
2018-12-02 19:16:39 +0100

    [1][ADMIN] Create Admin app

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div Welcome to the admin Dashboard {{ login }} !
+
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      test: '',
+    };
+  },
+  computed: {
+    ...mapGetters(['login']),
+  },
+  methods: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+</style>
